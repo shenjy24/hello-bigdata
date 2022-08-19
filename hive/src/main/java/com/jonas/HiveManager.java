f1ba6cbfa541b952d7fb11b035869b626921c189
shenjy <shenjy24@gmail.com>
2022-08-19 19:42:51 +0800

    hive demo

---
+++ hive/src/main/java/com/jonas/HiveManager.java
@@ -0,0 +1,96 @@
+package com.jonas;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class HiveManager {
+
+    private static final String DRIVER = "org.apache.hive.jdbc.HiveDriver";
+    private static final String URL = "jdbc:hive2://localhost:10000/hive";
+    private static final String USER = "jia";
+    private static final String PASSWORD = "123456";
+
+    static {
+        try {
+            Class.forName(DRIVER);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void createDatabase() throws SQLException {
+        String sql = "create database test_db";
+        try (Connection connection = getConnection()) {
+            try (Statement statement = connection.createStatement()) {
+                statement.execute(sql);
+            }
+        }
+    }
+
+    public static List<String> showDatabases() throws SQLException {
+        List<String> databases = new ArrayList<>();
+        String sql = "show databases";
+        try (Connection connection = getConnection()) {
+            try (Statement statement = connection.createStatement();
+                 ResultSet resultSet = statement.executeQuery(sql)) {
+                while (resultSet.next()) {
+                    databases.add(resultSet.getString(1));
+                }
+            }
+        }
+        return databases;
+    }
+
+    public static void createTable() throws SQLException {
+        String sql = "create table user_tb(id int, name string) row format delimited fields terminated by ','";
+        try (Connection connection = getConnection()) {
+            try (Statement statement = connection.createStatement()) {
+                statement.execute(sql);
+            }
+        }
+    }
+
+    public static List<String> showTables() throws SQLException {
+        List<String> tables = new ArrayList<>();
+        String sql = "show tables";
+        try (Connection connection = getConnection()) {
+            try (Statement statement = connection.createStatement();
+                 ResultSet resultSet = statement.executeQuery(sql)) {
+                while (resultSet.next()) {
+                    tables.add(resultSet.getString(1));
+                }
+            }
+        }
+        return tables;
+    }
+
+    public static Map<String, String> descTable(String table) throws SQLException {
+        Map<String, String> info = new HashMap<>();
+        String sql = "desc " + table;
+        try (Connection connection = getConnection()) {
+            try (Statement statement = connection.createStatement();
+                 ResultSet resultSet = statement.executeQuery(sql)) {
+                while (resultSet.next()) {
+                    info.put(resultSet.getString(1), resultSet.getString(2));
+                }
+            }
+        }
+        return info;
+    }
+
+    public static void loadData(String file) throws SQLException {
+        String sql = String.format("load data local inpath '%s' overwrite into table user_tb", file);
+        try (Connection connection = getConnection()) {
+            try (Statement statement = connection.createStatement()) {
+                statement.execute(sql);
+            }
+        }
+    }
+
+    private static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+}
---
+++ hive/src/test/java/com/jonas/AppTest.java
@@ -0,0 +1,50 @@
+package com.jonas;
+
+import lombok.SneakyThrows;
+import org.junit.Test;
+
+import java.util.List;
+import java.util.Map;
+
+public class AppTest {
+
+    @Test
+    @SneakyThrows
+    public void testCreateDatabase() {
+        HiveManager.createDatabase();
+    }
+
+    @Test
+    @SneakyThrows
+    public void showDatabases() {
+        List<String> databases = HiveManager.showDatabases();
+        System.out.println(databases);
+    }
+
+    @Test
+    @SneakyThrows
+    public void createTable() {
+        HiveManager.createTable();
+    }
+
+    @Test
+    @SneakyThrows
+    public void showTables() {
+        List<String> tables = HiveManager.showTables();
+        System.out.println(tables);
+    }
+
+    @Test
+    @SneakyThrows
+    public void descTable() {
+        Map<String, String> info = HiveManager.descTable("user_tb");
+        System.out.println(info);
+    }
+
+    @Test
+    @SneakyThrows
+    public void loadData() {
+        String file = "D:\\java\\jackal-bigdata\\hive\\src\\main\\resources\\user.txt";
+        HiveManager.loadData(file);
+    }
+}
